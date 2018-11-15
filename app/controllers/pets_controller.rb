2a3c11a6ede4b94b244db1cfe99383ed2bad9b5b
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-15 16:45:40 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,8 @@ class PetsController < ApplicationController
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
     
-    @pet.update(params["pet"])
+    @pet.update(params[:pet])
+    
     
     if params["owner"]["name"]
       
