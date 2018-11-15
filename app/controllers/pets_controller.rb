193176624238f9904ce542650d53f2760c94cb75
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-11-15 16:52:24 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -31,7 +31,7 @@ class PetsController < ApplicationController
 
     
     if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
+      @pet.owner = Owner.create(name: params["owner"]["name"]) || Pet.last.owner.name(params["owner"]["name"])
     end
     @pet.save
     redirect "pets/#{@pet.id}"
